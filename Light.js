ce9810744703f3727060929dbadb71eaa06e121e
xelada <45924261+Xeladarocks@users.noreply.github.com>
2020-06-23 02:20:02 -0400

    Update Light.js

--- Light.js
+++ Light.js
@@ -1,9 +1,9 @@
 class Light {
-    static __type__ = this.name;
     constructor(ltype, intensity, position) {
-        this.__type__ = Light.__type__;
+        this.__type__ = Light.name;
 		this.ltype = ltype;
 		this.intensity = intensity;
 		this.position = position;
 	}
 }
+Light.__type__ = Light.name;
